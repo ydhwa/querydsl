cb3ddd91757acaaf575798a34fcdf65ee5903ffd
YDH <ydhwa_18@naver.com>
2020-01-19 23:47:07 +0900

    JPQL and Querydsl

--- src/test/java/study/querydsl/entity/MemberTest.java
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -2,19 +2,14 @@ package study.querydsl.entity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-@Commit
 class MemberTest {
 
     @PersistenceContext
